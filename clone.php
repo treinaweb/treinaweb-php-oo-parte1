ef449b642efe1399d5236eed20cc53ebcc02e52c
Elton Fonseca <elton869@gmail.com>
2020-02-07 19:39:20 -0300

    10 - Usando operador clone

---
+++ clone.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once "src/Classes/Produto.php";
+
+$prod1 = new Produto;
+$prod1->titulo = "Skol";
+$prod1->descricao = "Cerveja Pilsen";
+$prod1->preco = 2.50;
+
+$prod2 = clone $prod1;
+$prod2->preco = 3.40;
+
+function alterarProduto($produto)
+{
+    $produto->titulo = "Skol Litão";
+
+    return $produto;
+}
+
+$prod3 = alterarProduto(clone $prod1);
+
+var_dump($prod1);
+var_dump($prod2);
+var_dump($prod3);
+
+
