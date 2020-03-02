2226cff720898beb2dff4e2d63e9709ce995de2a
Elton Fonseca <elton869@gmail.com>
2020-03-02 10:43:20 -0300

    19 - Modo estrito

--- src/Classes/Fornecedor.php
+++ src/Classes/Fornecedor.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 class Fornecedor 
 {
     public const PAIS = "Brasil";
--- tipos.php
+++ tipos.php
@@ -1,11 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 require_once "src/Classes/Fornecedor.php";
 
 $fornecedor = new Fornecedor;
 
 $fornecedor->cnpj = "123123123123";
-$fornecedor->nomeFantasia = "Meu armazem";
+$fornecedor->nomeFantasia = "Meu Armazem";
 $fornecedor->razaoSocial = "Meu armazem ltda";
 
 $fornecedor->autorizar(new Class {
